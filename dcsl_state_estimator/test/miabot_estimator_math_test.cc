9fdc54ba4d4618954e9d295ab7109c91540f950d
Will Scott <wlscott@princeton.edu>
2013-01-10 21:06:48 -0500

    added another (successful) test for estimator

--- dcsl_state_estimator/test/miabot_estimator_math_test.cc
+++ dcsl_state_estimator/test/miabot_estimator_math_test.cc
@@ -1,15 +1,16 @@
 #include "../src/dcsl_miabot_estimator_math.h"
 #include <gtest/gtest.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+// above two lines allow us to use M_PI for pi
 
 using namespace Eigen;
 
-TEST(MiabotEstimatorMath, statePropagation){
+TEST(StatePropagation, noTimeElapsed){
 	// declare some vectors to send to function
 	double dt;
 	Vector3d x, x_plus;
 	Vector2d u;
-
-	// choose an error bound (since trig calculations may not be exact)
 
 	// x shouldn't change for dt = 0
 	dt = 0;
@@ -30,6 +31,13 @@ TEST(MiabotEstimatorMath, statePropagation){
 	u << 0.0, 1.0;
 	miabot_propagate_state(x, u, dt);
 	EXPECT_TRUE(x == x_plus);
+}
+
+TEST(StatePropagation, straightLineMotion){
+	// declare some vectors to send to function
+	double dt;
+	Vector3d x, x_plus;
+	Vector2d u;
 
 	// test straight line motion
 	dt = 1.0;
@@ -48,6 +56,35 @@ TEST(MiabotEstimatorMath, statePropagation){
 	EXPECT_TRUE(x == x_plus);
 
  }
+
+TEST(StatePropagation, spinningInPlace){
+	// declare some vectors to send to function
+	double dt;
+	Vector3d x, x_plus;
+	Vector2d u;
+
+	// test for v = 0, omega !=0
+	dt = 1.0;
+	u << 0.0, 1.0;
+	x << 0.0, 0.0, 0.0;
+	x_plus << 0.0, 0.0, 1.0;
+	miabot_propagate_state(x, u, dt);
+	EXPECT_TRUE(x == x_plus);
+	
+	u << 0.0, -1.0;
+	x << 0.0, 0.0, 0.0;
+	x_plus << 0.0, 0.0, -1.0;
+	miabot_propagate_state(x, u, dt);
+	EXPECT_TRUE(x == x_plus);
+
+	u << 0.0, 1.0;
+	x << 1.0, 2.0, 0.0;
+	x_plus << 1.0, 2.0, 1.0;
+	miabot_propagate_state(x, u, dt);
+	EXPECT_TRUE(x == x_plus);
+
+ }
+
 
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
