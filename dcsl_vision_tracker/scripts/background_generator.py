06a865c757ae3219d904be06af1585658ae398b0
Brendan Andrade <bandrade@princeton.edu>
2013-01-19 23:05:48 -0500

    Merged lasted background generator and miabot tracker from master

--- dcsl_vision_tracker/scripts/background_generator.py
+++ dcsl_vision_tracker/scripts/background_generator.py
@@ -1,45 +1,63 @@
 #!/usr/bin/env python
+
+## @file
+# A node that generators a background image. It averages the first five images received on camera/image_raw and saves them to the location indicated by the /background_generator/image_name parameter.
+
+## @author Brendan Andrade
+
 import roslib; roslib.load_manifest('dcsl_vision_tracker')
 import rospy
 import cv
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
+## This class allows the generator of a background image for use with miabot_tracker.py
 class background_generator:
-
+    ## Creates the subscriber, sets properties defining how many images to average, and creates teh CvBridge object.
     def __init__(self):
         self.image_sub = rospy.Subscriber("/camera/image_raw", Image, self.callback)
         self.bridge = CvBridge()
         self.n = 5
         self.count = 1
 
+    ## Callback function for the image subscriber. It adds the received image to the average image and outputs the background image once self.n images have been added.
+    #
+    # @param data is the iamge data from the image subscriber.
     def callback(self,data):
+        # Bridge the image from ROS to OpenCV
         try:
             cv_image = self.bridge.imgmsg_to_cv(data, "mono8")
         except CvBridgeError, e:
             print e
             
+        # Create the background image object if this is the first trigger of the callback
         if self.count == 1:
             print "Initializing image"
             self.background_image = cv.CreateMat(cv_image.rows, cv_image.cols, cv.CV_64FC1)
             cv.SetZero(self.background_image)
-
+        
+        # Add image to total image (Pixel by pixel addition of grayscale values
         cv.Acc(cv_image,self.background_image)
         
+        # If self.n images have been added, take average and output
         if self.count == self.n:
-            
+            # For each pixel divide by self.n to find average image
             cv.ConvertScale(self.background_image,self.background_image,1.0/float(self.n));
+            # Get image name and output image
             image_name = rospy.get_param('/background_generator/image_name')
             cv.SaveImage(image_name,self.background_image)  
             print "Background image generated...node shutting down"
+            # Shutdown node
             rospy.signal_shutdown("Background image generated...node shutting down")
 
         self.count += 1
-
+        #Display image in window
         cv.ShowImage("Image Window", self.background_image)
         cv.WaitKey(3)
 
-        
+## Main function triggered when node begins.
+#
+# Creates the node object and background_generator object.        
 def main():
     rospy.init_node('background_generator')
     bg = background_generator()
--- dcsl_vision_tracker/scripts/miabot_tracker.py
+++ dcsl_vision_tracker/scripts/miabot_tracker.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+
+## @file
+# A node that subscribes to the /camera/image_raw topic with Image messages, uses OpenCV to find the poses of the robots in the images, and publishes the poses of the robots as a PoseArray. It also subscribes to the state_estimate topic (PoseArray messages) so that the published PoseArray maintains the same order as the state_estimate. It matches the detected robots with the estimated poses using the Hungarian algorithm via the munkres library.
+# This node will soon be depricated in favor of a node based on the dcsl_vision_tracker_API which is under development.
+
+## @author Brendan Andrade
+
 import math as m
 
 from munkres import Munkres
@@ -11,9 +18,10 @@ import cv
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge,CvBridgeError
 
-
+## This class allows detection of robots in an image,  orders these readings based on state estimates, and publishes them as a PoseArray.
 class miabot_tracker:
-
+    
+    ## Creates publishers and subscribers, loads background image and nRobots parameter, and creates CvBridge object.
     def __init__(self):
         self.image_pub = rospy.Publisher("tracked_image",Image)
         self.measurement_pub = rospy.Publisher("planar_measurements", PoseArray)
@@ -24,21 +32,29 @@ class miabot_tracker:
         self.bridge = CvBridge()
         self.nRobots = rospy.get_param("/n_robots",1)
 
+    ## Callback function for when new images are received. Senses positions of robots, sorts them into the correct order, publishes readings, and displays image.
+    #
+    # @param data is the image data received from the /camera/image_raw topic
     def imageCallback(self,data):
+        # Bridge image from ROS message to OpenCV
         try:
             working_image = self.bridge.imgmsg_to_cv(data, "mono8")
         except CvBridgeError, e:
             print e
 
-        #Create BGR8 image to be output
+        # Create BGR8 image to be output
         output_image = cv.CreateMat(working_image.height,working_image.width,cv.CV_8UC3)
         cv.Merge(working_image,working_image,working_image, None, output_image)        
-
-        cv.AbsDiff(working_image,self.background,working_image) #Background subtractionstorage
+        
+        # Background subtraction to leave robots only in image
+        cv.AbsDiff(working_image,self.background,working_image)
+        # Threshold to convert to binary image
         threshold = 100
         cv.Threshold(working_image,working_image,threshold,255,cv.CV_THRESH_BINARY) #Threshold to create binary image
+        # Erode image to remove small holes in binary image
         erodeIterations = 1
         cv.Erode(working_image, working_image, None, erodeIterations)
+        # Create contour objects mapping the boundaries between white and black in the binary image
         contours = cv.FindContours(working_image, cv.CreateMemStorage(), cv.CV_RETR_LIST, cv.CV_CHAIN_APPROX_SIMPLE, (0,0))
 
         red = cv.RGB(255,0,0)
@@ -98,9 +114,9 @@ class miabot_tracker:
                     
                 #Cycle to next contour
                 _c = _c.h_next() 
-
+            #Draw contours on output image
             cv.DrawContours(output_image, contours, red, blue,2)
-
+        #Display image
         cv.ShowImage("Image Window", output_image)
         cv.WaitKey(3)
 
@@ -113,10 +129,18 @@ class miabot_tracker:
 
         self.measurement_pub.publish(self.measurements)
 
+    ## Callback function for state estimates. Stores state estimates as self.currentStates.
+    #
+    # @param data is the message received from the subscriber.
     def stateCallback(self, data):
         self.currentStates = data       
 
+    ## Matchings readings with the robots in the current state estimate and stores the sorted measurements in a PoseArray called self.measurements.
+    # 
+    # To match the readings with the robots. A cost matrix is construsted of the square of the distance between each state estimate and each reading found in the image. The Hungarian algorithm is used to find the lowest cost set of matches between the readings and state_estimates. It also rejects false positive readings if they are far from the estimated positions of the robots and can handle the case when fewer robots are detected in the image than are known to exist in the system.
+    # @param readings is an unsorted list of tuples represented detected poses of robots.
     def matchRobots(self, readings):
+        # Create least squares cost matrix.
         costMatrix = []
         for reading in readings:
             temp = []
@@ -124,9 +148,12 @@ class miabot_tracker:
                 difference = pow(reading[0]-state.position.x,2)+pow(reading[1]-state.position.y,2)
                 temp.append(difference)                
             costMatrix.append(temp)
+        # Apply Hungarian algorithm to cost matrix if cost matrix exists, i.e. if robots are detected in the image.
         if len(costMatrix) != 0:
             mun = Munkres()
-            indexes = mun.compute(costMatrix)
+            # indexes is a list of the matched readings and estimates. Each entry is a tuple (X,Y) where X is the index of the reading and Y is the index of the state_estimate
+            indexes = mun.compute(costMatrix) 
+            # Sort readings into measurements PoseArray in correct order
             for i in xrange(0,self.nRobots):
                 assigned = False
                 for pair in indexes:
@@ -139,17 +166,19 @@ class miabot_tracker:
                         p.orientation.w = 1
                         self.measurements.poses.append(p)
                         assigned = True
+                # Case for undetected robot
                 if not assigned:
                     p = Pose()
-                    p.orientation.w = 0
+                    p.orientation.w = 0 # Use the orientation.w entry as a binary signifier whether or not that robot has been detected in the image
                     self.measurements.poses.append(p)
+        # If not robots detected return all orientation.w as zero
         else:
             for i in xrange(0,self.nRobots):
                 p = Pose()
                 p.orientation.w = 0
                 self.measurements.poses.append(p)
 
-
+## Runs on the startup of the node. Initializes the node and creates the MiabotTracker object.
 def main():
     rospy.init_node('dcsl_miabot_tracker')
     tracker = miabot_tracker()
@@ -164,4 +193,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-## http://www.ros.org/wiki/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
+# http://www.ros.org/wiki/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
