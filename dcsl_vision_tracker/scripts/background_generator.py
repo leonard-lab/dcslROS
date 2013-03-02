e9483f37bf27e1ee540ed86761014d20715239eb
Brendan Andrade <bandrade@princeton.edu>
2013-03-02 00:51:32 -0500

    Updated the background generator for color images and make launch file for beluga background generation.

--- dcsl_vision_tracker/scripts/background_generator.py
+++ dcsl_vision_tracker/scripts/background_generator.py
@@ -8,17 +8,19 @@
 import roslib; roslib.load_manifest('dcsl_vision_tracker')
 import rospy
 import cv
+import sys
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 ## This class allows the generator of a background image for use with miabot_tracker.py
 class background_generator:
     ## Creates the subscriber, sets properties defining how many images to average, and creates teh CvBridge object.
-    def __init__(self):
+    def __init__(self, image_type):
         self.image_sub = rospy.Subscriber("/camera/image_raw", Image, self.callback)
         self.bridge = CvBridge()
         self.n = 5
         self.count = 1
+        self.image_type = image_type
 
     ## Callback function for the image subscriber. It adds the received image to the average image and outputs the background image once self.n images have been added.
     #
@@ -26,47 +28,44 @@ class background_generator:
     def callback(self,data):
         # Bridge the image from ROS to OpenCV
         try:
-            cv_image = self.bridge.imgmsg_to_cv(data, "mono8")
+            cv_image = self.bridge.imgmsg_to_cv(data, self.image_type)
         except CvBridgeError, e:
             print e
             
         # Create the background image object if this is the first trigger of the callback
         if self.count == 1:
             print "Initializing image"
-            self.background_image = cv.CreateMat(cv_image.rows, cv_image.cols, cv.CV_64FC1)
-            cv.SetZero(self.background_image)
-        
-        # Add image to total image (Pixel by pixel addition of grayscale values
-        cv.Acc(cv_image,self.background_image)
-        
-        # If self.n images have been added, take average and output
+            self.background_image = cv.CloneMat(cv_image)
+        # Otherwise add image to running average
+        else:
+            cv.AddWeighted(self.background_image, float(self.count-1)/float(self.count), cv_image, 1.0/float(self.count), 0, self.background_image)
+                       
+        # If self.n images have been added, output
         if self.count == self.n:
-            # For each pixel divide by self.n to find average image
-            cv.ConvertScale(self.background_image,self.background_image,1.0/float(self.n));
             # Get image name and output image
-            image_name = rospy.get_param('/background_generator/image_name')
-            cv.SaveImage(image_name,self.background_image)  
+            image_name = rospy.get_param('~image_name')
+            print image_name
+            cv.SaveImage(image_name, self.background_image)  
             print "Background image generated...node shutting down"
             # Shutdown node
             rospy.signal_shutdown("Background image generated...node shutting down")
 
         self.count += 1
-        #Display image in window
-        cv.ShowImage("Image Window", self.background_image)
-        cv.WaitKey(3)
+
 
 ## Main function triggered when node begins.
 #
 # Creates the node object and background_generator object.        
-def main():
+def main(image_type):
     rospy.init_node('background_generator')
-    bg = background_generator()
+    bg = background_generator(image_type)
     try:
         rospy.spin()
     except KeyboardInterrupt:
         print "Shutting down"
-    cv.DestroyAllWindows()
 
 if __name__ == '__main__':
     print "Starting background generator"
-    main()
+    args = rospy.myargv(argv=sys.argv)
+    image_type = args[1]
+    main(image_type)
