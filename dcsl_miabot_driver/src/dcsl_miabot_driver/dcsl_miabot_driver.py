b8eaf67605ac54e809b4ff16f4eb7d9efd92caa8
Brendan Andrade <bandrade@princeton.edu>
2013-06-10 23:40:37 -0400

    Miabot driver updated. Untested.

--- dcsl_miabot_driver/src/dcsl_miabot_driver/dcsl_miabot_driver.py
+++ dcsl_miabot_driver/src/dcsl_miabot_driver/dcsl_miabot_driver.py
@@ -5,7 +5,7 @@
 
 ## @author Brendan Andrade
 
-import math, serial, sys
+import sys
 
 # Not used with catkin
 # import roslib; roslib.load_manifest('dcsl_miabot_driver')
@@ -13,62 +13,47 @@ import math, serial, sys
 import rospy
 from geometry_msgs.msg import Twist
 
+from dcsl_miabot_driver.dcsl_miabot_API import *
 
-## The callback function for when a Twist message is published on the cmd_vel topic.
+
+##
 #
-# This function pulls the commands from the Twist message, converts them into wheel speeds for the robot, and then writes the wheel speed command to the serial port.
-# @param cmd_vel is the message object received by the subscriber.
-def callback(cmd_vel):
-    # Parameters
-    diffConversionFactor = 0.1 # Distance between the wheels in meters
-    motorScaleFactor = 1000 # Commands are received in the Twist message in meters and the robot accepts them in mm. This is the scale factor.
+#
+class MiabotNode(object):
+    ##
+    #
+    #
+    def __init__(self, port):
+        self.sub = rospy.Subscriber("cmd_vel", Twist, self.callback)
+        self.miabot = Miabot(port)
+        
 
-    # Receive commanded velocity and angular velocity from message
-    targetTransVel = cmd_vel.linear.x
-    targetRotVel = cmd_vel.angular.z
-
-    # Difference in speed between the wheels as this is a differential drive robot
-    rotTerm = (math.pi/180.0) * targetRotVel / diffConversionFactor
-
-    # Motor speeds in mm/s
-    leftVel = int((targetTransVel - rotTerm) * motorScaleFactor)
-    rightVel = int((targetTransVel + rotTerm) * motorScaleFactor)
-
-    # Create string for commanding wheel speed and write it to the serial port.
-    motorCommand = '[=<' + str(leftVel) + '><' + str(rightVel) + '>]' + '\n'
-    ser.write(motorCommand)
-
+    ## The callback function for when a Twist message is published on the cmd_vel topic.
+    #
+    # This function pulls the commands from the Twist message, converts them into wheel speeds for the robot, and then writes the wheel speed command to the serial port.
+    # @param cmd_vel is the message object received by the subscriber.
+    def callback(self, cmd_vel):
+        self.miabot.sendMotorCommand(cmd_vel.linear.x, cmd_vel.angular.z)
+        
+    ##
+    #
+    #
+    def shutdown(self):
+        self.miabot.stop()
+        self.miabot.close()
     
 ## Main function which is called when the node begins
 #
-# Initializes the node and the subscriber
-def main():
+# Initializes the node and creates miabot_node object
+def main(port):
     rospy.init_node('dcsl_miabot',anonymous=True)
-    rospy.Subscriber("cmd_vel", Twist, callback)
+    miabot_node = MiabotNode(port)
     rospy.spin()
+    rospy.on_shutdown(MiabotNode.shutdown())
 
-## This function is triggered when the node shuts down.
-#
-# Turns off the motor and closes the serial port.
-def closePort():
-    leftVel = 0
-    rightVel = 0
-    motorCommand = '[=<' + str(leftVel) + '><' + str(rightVel) + '>]' + '\n'
-    ser.write(motorCommand)
-    ser.close()
 
 if __name__ == "__main__":
     args = rospy.myargv(argv=sys.argv)
-    ser = serial.Serial()
     port = args[1]
-    rospy.loginfo("Connecting to serial port at %s", port)
-    ser.port = port
-    ser.baudrate = 19200
-    ser.timeout = 600
-    ser.bytesize = serial.EIGHTBITS
-    ser.stopbits = serial.STOPBITS_ONE
-    ser.parity = serial.PARITY_NONE
-    ser.open()
-    main()
-    rospy.on_shutdown(closePort())
+    main(port)
         
