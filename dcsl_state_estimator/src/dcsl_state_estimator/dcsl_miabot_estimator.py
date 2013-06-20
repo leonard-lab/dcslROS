8ca39be5d00ddd5b56185d24ca63f576b17ee294
Brendan Andrade <bandrade@princeton.edu>
2013-06-19 22:48:20 -0400

    Implemented msg to array methods

--- dcsl_state_estimator/src/dcsl_state_estimator/dcsl_miabot_estimator.py
+++ dcsl_state_estimator/src/dcsl_state_estimator/dcsl_miabot_estimator.py
@@ -6,9 +6,12 @@
 ## @author Brendan Andrade
 
 import rospy
-from geometry_msgs.msg import PoseArray, Pose
-from dcsl_messages.msg import TwistArray, StateArray
+from geometry_msgs.msg import PoseArray
+from dcsl_messages.msg import TwistArray, StateArray, State
 from dcsl_state_estimator.ekf_API import ekf
+
+import numpy as np
+
 ##
 #
 #
@@ -22,7 +25,8 @@ class MiabotEstimator:
         self.input_sub = rospy.Subscriber("cmd_vel_array", TwistArray, self.input_callback)
         self.pub = rospy.Publisher("state_estimate", StateArray)
         
-        self.ekf = ekf(
+        self.ekfs = None
+
     ##
     #
     #
@@ -38,7 +42,37 @@ class MiabotEstimator:
     ##
     #
     #
-    
+    def _pose_to_z_array(self, pose):
+        z = np.zeros(4)
+        z[0] = pose.position.x
+        z[1] = pose.position.y
+        z[2] = pose.position.z
+        z[3] = pose.orientation.z
+        return z
+
+    ##
+    #
+    #
+    def _twist_to_u_array(self, twist):
+        u = np.zeros(3)
+        u[0] = twist.linear.x
+        u[1] = twist.angular.z
+        u[2] = twist.linear.z
+        return u
+
+    ##
+    #
+    #
+    def _x_array_to_state(self, x):
+        state = State()
+        state.pose.position.x = x[0]
+        state.pose.position.y = x[1]
+        state.pose.position.z = x[2]
+        state.pose.orientation.z = x[5]
+        state.twist.linear.x = x[3]
+        state.twist.linear.z = x[4]
+        state.twist.angular.z = x[6]
+        return state
     
 def main():
     rospy.init_node('dcsl_miabot_estimator')
