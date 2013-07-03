02af05f0123b99c109ed48f3e712da108cd4399b
Brendan Andrade <brnd4n@gmail.com>
2013-07-03 12:49:06 -0400

    Updated planar passthrough estimator to use StateArray messages.

--- dcsl_state_estimator/src/dcsl_state_estimator/estimate_by_planar_passthrough.py
+++ dcsl_state_estimator/src/dcsl_state_estimator/estimate_by_planar_passthrough.py
@@ -5,8 +5,8 @@
 # roslib.load_manifest('dcsl_state_estimator')
 
 import rospy
-from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseArray
+from dcsl_messages.msg import State, StateArray
 from std_msgs.msg import Float32
 
 class PassthroughEstimator:
@@ -14,17 +14,18 @@ class PassthroughEstimator:
     def __init__(self):
         self.depth_sub = rospy.Subscriber("depth_meters", Float32, self.depth_callback)
         self.planar_sub = rospy.Subscriber("planar_measurements", PoseArray, self.planar_callback)
-        self.pub = rospy.Publisher("state_estimate", PoseArray)
+        self.pub = rospy.Publisher("state_estimate", StateArray)
 
     def depth_callback(self, data):
         self.depth = data.data
 
     def planar_callback(self, data):
         if data.poses[0].orientation.w == 1:
-            measured_pose = data.poses[0]
-            measured_pose.position.z = self.depth
-            array = PoseArray()
-            array.poses.append(measured_pose)
+            measured_state = State()
+            measured_state.pose = data.poses[0]
+            measured_state.pose.position.z = self.depth
+            array = StateArray()
+            array.states.append(measured_state)
             array.header.stamp = rospy.Time.now()
             self.pub.publish(array)
 
