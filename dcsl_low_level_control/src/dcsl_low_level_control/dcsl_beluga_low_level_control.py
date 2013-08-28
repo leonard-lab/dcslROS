9f6905490b284922509430ef57ef5bd22a0377e6
Brendan Andrade <brnd4n@gmail.com>
2013-08-27 21:31:30 -0400

    Integrated joystick control into low level controller. Addresses #30 for Beluga system

--- dcsl_low_level_control/src/dcsl_low_level_control/dcsl_beluga_low_level_control.py
+++ dcsl_low_level_control/src/dcsl_low_level_control/dcsl_beluga_low_level_control.py
@@ -14,6 +14,8 @@ import sys
 import rospy
 from geometry_msgs.msg import PoseArray
 from dcsl_messages.msg import TwistArray, StateArray, State, BelugaArray, belugaInput
+from dynamic_reconfigure.server import Server
+from dcsl_low_level_control.cfg import dcsl_beluga_low_level_control_configConfig as Config
 
 ##
 #
@@ -23,13 +25,22 @@ class BelugaLowLevelController(object):
     ##
     #
     #
-    def __init__(self):
+    def __init__(self, n_robots):
+
+        self.n_robots = n_robots
 
         self.mode = None
         self.current_commands = None
+        self.state_data = None
+        self.last_pub = -10000000
 
         # Setup publisher for commands to belugas
         self.input_pub = rospy.Publisher("cmd_inputs", BelugaArray)
+
+        # Setup dynamic reconfigure server to give joystick selections
+        self.srv = Server(Config, self.joystick_selection_callback)
+        self.active_joy = -1
+        self.joy_command = None
 
         # Subscribe to current state information
         self.estimate_sub = rospy.Subscriber("state_estimate", StateArray, self.estimate_callback)
@@ -38,6 +49,21 @@ class BelugaLowLevelController(object):
         self.direct_sub = rospy.Subscriber("direct_input", BelugaArray, self.direct_callback)
         self.velocity_sub = rospy.Subscriber("velocity_input", TwistArray, self.velocity_callback)
         self.wp_sub = rospy.Subscriber("wp_input", PoseArray, self.wp_callback)
+        
+        # Subscribe to joystick
+        self.joystick_sub = rospy.Subscriber("joy_cmd", belugaInput, self.joy_callback)
+
+    ##
+    #
+    #
+    def joystick_selection_callback(self, config, level):
+        self.active_joy = int(config["JoystickRobot"])
+        return config
+    ##
+    #
+    #
+    def joy_callback(self, data):
+        self.joy_command = data
 
     ##
     #
@@ -45,13 +71,15 @@ class BelugaLowLevelController(object):
     def direct_callback(self, data):
         self.mode = 0
         self.current_commands = data
-    
+        self.publish_inputs()
+
     ##
     #
     #
     def velocity_callback(self, data):
         self.mode = 1
         self.current_commands = data
+        self.publish_inputs()
 
     ##
     #
@@ -67,26 +95,48 @@ class BelugaLowLevelController(object):
             wp[3] = pose.orientation.z
             u_wp.append(wp)
         self.current_commands = u_wp
+        self.publish_inputs()
 
     ##
     #
     #
     def estimate_callback(self, data):
+        self.state_data = data
+        if rospy.get_time() > self.last_pub+0.05:
+            self.publish_inputs()
+    
+    ##
+    #
+    #
+    def publish_inputs(self):
         if self.mode == 0:
-            self.input_pub.publish(self.current_commands)
-        elif self.mode == 1 or self.mode == 2:
-            direct_array = BelugaArray()
-            for i, state in enumerate(data.states):
+            beluga_input_array = self.current_commands
+        elif self.state_data is not None:
+            beluga_input_array = BelugaArray()
+            # Consider service call here to get most recent state data
+            for i, state in enumerate(self.state_data.states):
                 x = self._state_to_mat(state)
                 if self.mode == 1:
                     u_direct = self.velocity_controller(x, self.current_commands)
                 elif self.mode == 2:
                     u_direct = self.wp_controller(x, self.current_commands)
+                else:
+                    u_direct = np.array([0., 0., 0.])
                 beluga_input = self._u_direct_to_beluga_input(u_direct)
-                direct_array.append(beluga_input)
-            direct_array.header = rospy.get_time()
-            self.input_pub.publish(direct_array)
-                
+                beluga_input_array.belugas.append(beluga_input)
+        else:
+            beluga_input_array = BelugaArray()
+            for i in xrange(0, self.n_robots):
+                u_direct = np.array([0., 0., 0.])
+                beluga_input = self._u_direct_to_beluga_input(u_direct)
+                beluga_input_array.belugas.append(beluga_input)
+        # Override any supplied control with joystick
+        if self.active_joy >= 0 and self.active_joy < self.n_robots and self.joy_command is not None:
+            beluga_input_array.belugas[self.active_joy] = self.joy_command
+        # Publish
+        self.input_pub.publish(beluga_input_array)
+        self.last_pub = rospy.get_time()
+            
     ## 
     #
     #
@@ -105,8 +155,8 @@ class BelugaLowLevelController(object):
     ##
     #
     #
-    def _u_direct_to_beluga_input(u):
-        b = belugaInput
+    def _u_direct_to_beluga_input(self, u):
+        b = belugaInput()
         b.thrust_motor = u[0]
         b.servo = u[1]
         b.vertical_motor = u[2]
@@ -114,8 +164,10 @@ class BelugaLowLevelController(object):
 
 def main():
     rospy.init_node("beluga_low_level_controller")
-    
-    controller = BelugaLowLevelController()
+
+    n_robots = sys.argv[1]
+
+    controller = BelugaLowLevelController(n_robots)
     
     rospy.spin()
 
